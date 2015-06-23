cf95377a7f5932ef525a3abdfe464edef4b0198d
amaro <amaro@users.noreply.github.com>
2015-06-23 12:03:11 -0400

    test_client|server, implemented server writes

--- test_client.py
+++ test_client.py
@@ -15,9 +15,21 @@ def server_sends(option, opt, value, parser):
 
   sys.exit(0)
 
+def server_writes(option, opt, value, parser):
+  # needed keys don't alter the result of the benchmark because we don't
+  # have to send those to the client. only the output keys have an impact
+  # on the benchmark
+  for output_keys in ENTRIES:
+    for exp in range(NUM_REPETITION):
+      time.sleep(0.25)
+      exe("./client3 -w -n 1024 -o {0}".format(output_keys))
+
+  sys.exit(0)
+
 def main():
   parser = optparse.OptionParser()
   parser.add_option('--server-sends', action='callback', callback=server_sends)
+  parser.add_option('--server-writes', action='callback', callback=server_writes)
 
   (options, args) = parser.parse_args()
 
--- test_server.py
+++ test_server.py
@@ -20,9 +20,27 @@ def server_sends(option, opt, value, parser):
 
   sys.exit(0)
 
+def server_writes(option, opt, value, parser):
+  # needed keys don't alter the result of the benchmark because we don't
+  # have to send those to the client. only the output keys have an impact
+  # on the benchmark
+  for output_keys in ENTRIES:
+    cmd = "perf stat -e {0} -x, -r {1} taskset -c 11 ./server3 -w -n 1024 -o {2}".format(
+                                            PERF_EVENTS, NUM_REPETITION, output_keys)
+    out = exe(cmd)
+    times = get_elapsed(out)
+    print "num output keys={0}, avg={1}".format(output_keys, avg(times))
+    print "cache-references={0}".format(get_perf_result(out, "cache-references"))
+    print "cache-misses={0}".format(get_perf_result(out, "cache-misses"))
+    print "cycles={0}".format(get_perf_result(out, "cycles"))
+    print "\n"
+
+  sys.exit(0)
+
 def main():
   parser = optparse.OptionParser()
   parser.add_option('--server-sends', action='callback', callback=server_sends)
+  parser.add_option('--server-writes', action='callback', callback=server_writes)
 
   (options, args) = parser.parse_args()
 
