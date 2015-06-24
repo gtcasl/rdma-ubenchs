7488c775fa67340b10cf0131b0a7f2208de9f292
amaro <amaro@users.noreply.github.com>
2015-06-24 14:23:16 -0400

    added --client-computes option to python scripts

--- test_client.py
+++ test_client.py
@@ -26,10 +26,23 @@ def server_writes(option, opt, value, parser):
 
   sys.exit(0)
 
+def client_computes(option, opt, value, parser):
+  # output keys don't alter the result of the benchmark because we don't
+  # have to send those to the client. only the needed keys have an impact
+  # on the benchmark
+  for needed_keys in ENTRIES:
+    for exp in range(NUM_REPETITION):
+      time.sleep(0.25)
+      exe("./client3 -n 1024 -n {0} -o 1024".format(needed_keys))
+
+  sys.exit(0)
+
+
 def main():
   parser = optparse.OptionParser()
   parser.add_option('--server-sends', action='callback', callback=server_sends)
   parser.add_option('--server-writes', action='callback', callback=server_writes)
+  parser.add_option('--client-computes', action='callback', callback=client_computes)
 
   (options, args) = parser.parse_args()
 
--- test_server.py
+++ test_server.py
@@ -37,10 +37,28 @@ def server_writes(option, opt, value, parser):
 
   sys.exit(0)
 
+def client_computes(option, opt, value, parser):
+  # needed keys don't alter the result of the benchmark because we don't
+  # have to send those to the client. only the output keys have an impact
+  # on the benchmark
+  for needed_keys in ENTRIES:
+    cmd = "perf stat -e {0} -x, -r {1} taskset -c 11 ./server3 -n {2} -o 1024".format(
+                                            PERF_EVENTS, NUM_REPETITION, needed_keys)
+    out = exe(cmd)
+    times = get_elapsed(out)
+    print "num needed keys={0}, avg={1}".format(needed_keys, avg(times))
+    print "cache-references={0}".format(get_perf_result(out, "cache-references"))
+    print "cache-misses={0}".format(get_perf_result(out, "cache-misses"))
+    print "cycles={0}".format(get_perf_result(out, "cycles"))
+    print "\n"
+
+  sys.exit(0)
+
 def main():
   parser = optparse.OptionParser()
   parser.add_option('--server-sends', action='callback', callback=server_sends)
   parser.add_option('--server-writes', action='callback', callback=server_writes)
+  parser.add_option('--client-computes', action='callback', callback=client_computes)
 
   (options, args) = parser.parse_args()
 
